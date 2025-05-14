079160cfe97d100596e39f2feb978bbffaf33e6c
wub <wubingzheng@gmail.com>
2025-05-14 11:47:36 +0800

    with_log example

---
+++ examples/src/with_log.rs
@@ -0,0 +1,23 @@
+use atiour::status::Status;
+
+mod helloworld {
+    include!(concat!(env!("OUT_DIR"), "/helloworld.rs"));
+}
+
+use helloworld::*;
+
+struct MyGreeter();
+
+impl Greeter for MyGreeter {
+    fn say_hello(&self, req: HelloRequest) -> Result<HelloReply, Status> {
+        Ok(HelloReply {
+            message: format!("Hello {}!", req.name),
+        })
+    }
+}
+
+fn main() {
+    env_logger::init();
+
+    atiour::serve(GreeterServer::new(MyGreeter()), "127.0.0.1:50051").unwrap();
+}
